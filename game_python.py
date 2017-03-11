32aa1b7b1aaf14467f7ba2e2e760059aef5c7863
wcsten <gabriel_sten@hotmail.com>
2017-03-11 13:50:31 -0300

    add manipulate archives

--- game_python.py
+++ game_python.py
@@ -1,37 +1,34 @@
 import random
 
 
-def sort_questions(list_tiger, list_bear, list_turtle):
+def sort_questions(dict_tiger, dict_bear, dict_turtle):
 	list_question = []
 
-	question_number_tiger = random.randint(1, len(list_tiger))
+	question_number_tiger = random.randint(1, len(dict_tiger))
 	question_number_tiger -= 1
 
-	question_number_bear = random.randint(1, len(list_bear))
+	question_number_bear = random.randint(1, len(dict_bear))
 	question_number_bear -= 1
 
-	question_number_turtle = random.randint(1, len(list_turtle))
+	question_number_turtle = random.randint(1, len(dict_turtle))
 	question_number_turtle -= 1
 
-	for index, obj in enumerate(list_tiger):
-		print('for tiger')
+	for index, obj in enumerate(dict_tiger):
 		if index == question_number_tiger:
 			list_question.append(obj)
-			list_tiger.remove(obj)
+			dict_tiger.remove(obj)
 			break
 
-	for index, obj in enumerate(list_bear):
-		print('for bear')
+	for index, obj in enumerate(dict_bear):
 		if index == question_number_bear:
 			list_question.append(obj)
-			list_bear.remove(obj)
+			dict_bear.remove(obj)
 			break
 
-	for index, obj in enumerate(list_turtle):
-		print('for turtle')
+	for index, obj in enumerate(dict_turtle):
 		if index == question_number_turtle:
 			list_question.append(obj)
-			list_turtle.remove(obj)
+			dict_turtle.remove(obj)
 			break
 
 	return list_question
@@ -47,33 +44,37 @@ def choice_one_question(choice, list_question):
 if __name__ == '__main__':
 
 	list_tiger = [
-		'tiger 1',
-		'tiger 2',
-		'tiger 3',
-		'tiger 4',
-		'tiger 5',
+		{'question': 'tiger 1', 'category':'1'},
+		{'question': 'tiger 2', 'category':'1'},
+		{'question': 'tiger 3', 'category':'1'},
+		{'question': 'tiger 4', 'category':'1'},
+		{'question': 'tiger 5', 'category':'1'},
 	]
 
 	list_bear = [
-		'bear 1',
-		'bear 2',
-		'bear 3',
-		'bear 4',
-		'bear 5',
+		{'question': 'bear 1', 'category':'2'},
+		{'question': 'bear 2', 'category':'2'},
+		{'question': 'bear 3', 'category':'2'},
+		{'question': 'bear 4', 'category':'2'},
+		{'question': 'bear 5', 'category':'2'},
 	]
 
 	list_turtle = [
-		'turtle 1',
-		'turtle 2',
-		'turtle 3',
-		'turtle 4',
-		'turtle 5',
+		{'question': 'turtle 1', 'category':'3'},
+		{'question': 'turtle 2', 'category':'3'},
+		{'question': 'turtle 3', 'category':'3'},
+		{'question': 'turtle 4', 'category':'3'},
+		{'question': 'turtle 5', 'category':'3'},
 	]
 
 	result_list = []
+	email = input('Informe seu email para começar:')
+
 	#1
 	sorted_list1 = sort_questions(list_tiger, list_bear, list_turtle)
-	print(sorted_list1)
+	for item_dict in sorted_list1:
+		print(item_dict.get('question'))
+
 	choice1 = int(input('Escolha uma questão entre 1 e 3:'))
 	choice1 -= 1
 	second_answer1 = choice_one_question(choice1, sorted_list1)
@@ -81,7 +82,9 @@ if __name__ == '__main__':
 
 	#2
 	sorted_list2 = sort_questions(list_tiger, list_bear, list_turtle)
-	print(sorted_list2)
+	for item_dict in sorted_list2:
+		print(item_dict.get('question'))
+
 	choice2 = int(input('Escolha uma questão entre 1 e 3:'))
 	choice2 -= 1
 	second_answer2 = choice_one_question(choice2, sorted_list2)
@@ -89,7 +92,9 @@ if __name__ == '__main__':
 
 	#3
 	sorted_list3 = sort_questions(list_tiger, list_bear, list_turtle)
-	print(sorted_list3)
+	for item_dict in sorted_list3:
+		print(item_dict.get('question'))
+
 	choice3 = int(input('Escolha uma questão entre 1 e 3:'))
 	choice3 -= 1
 	second_answer3 = choice_one_question(choice3, sorted_list3)
@@ -97,7 +102,9 @@ if __name__ == '__main__':
 
 	#4
 	sorted_list4 = sort_questions(list_tiger, list_bear, list_turtle)
-	print(sorted_list4)
+	for item_dict in sorted_list4:
+		print(item_dict.get('question'))
+
 	choice4 = int(input('Escolha uma questão entre 1 e 3:'))
 	choice4 -= 1
 	second_answer4 = choice_one_question(choice4, sorted_list4)
@@ -105,19 +112,24 @@ if __name__ == '__main__':
 
 	#5
 	sorted_list5 = sort_questions(list_tiger, list_bear, list_turtle)
-	print(sorted_list5)
+	for item_dict in sorted_list5:
+		print(item_dict.get('question'))
+
 	choice5 = int(input('Escolha uma questão entre 1 e 3:'))
 	choice5 -= 1
 	second_answer5 = choice_one_question(choice5, sorted_list5)
 	result_list.append(second_answer5)
+	
 
-	print(result_list)
+	for item_dict in result_list:
+		question = item_dict.get('question')
+		print(question)
 
-	arquivo = open('Banco_de_questoes.txt', 'w')
-	arquivo.writelines(result_list)
-	arquivo.close()
 
-	arquivo = open('Banco_de_questoes.txt', 'r')
-	texto = arquivo.read()
-	print(texto)
-	arquivo.close()
+	database_path = 'banco_de_questoes/{}.csv'.format(email) 
+
+	with open(database_path, 'w') as file:
+		for item_dict in result_list:
+			line = '{},{}\n'.format(item_dict.get('question'), item_dict.get('category'))
+			file.write(line)
+
